d66ef43fb76f42eb894997ba3bd0c6338d5546a6
Git-QA1 <gittesting6@gmail.com>
2025-11-10 09:54:00 +0530

    Create calculate.py

---
+++ calculate.py
@@ -0,0 +1,7 @@
+def greet_and_calculate():
+   greeting = "Hello, welcome to Python!"
+   result = 5 * 7 # Simple calculation
+   print(greeting)
+   print(f"The result of 5 multiplied by 7 is {result}")
+#Call the function
+greet_and_calculate()
